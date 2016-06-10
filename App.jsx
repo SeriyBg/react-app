70fd9ac7eb65531c2877700fbafe02b3f478bd2a
Serg Bishyr <sbishyr@yukon.cv.ua>
2016-06-10 18:01:05 +0300

    Add state to components

--- App.jsx
+++ App.jsx
@@ -1,11 +1,36 @@
 import React from 'react';
 
 class App extends React.Component {
-   render() {
+	constructor(){
+		super();
+		this.state = {
+			data: [
+				{
+					"id":1,
+					"name":"Foo",
+					"age":20
+				},{
+					"id":2,
+					"name":"Bar",
+					"age":30					
+				},{
+					"id":3,
+					"name":"Baz",
+					"age":40
+				}
+			]
+			
+		}
+	}
+	render() {
       return (
          <div>
             <Header/>
-			<Content/>
+			<table>
+				<tbody>
+					{this.state.data.map((person, i) => <TableRow key={i} data={person} />)}
+				</tbody>
+			</table>
          </div>
       );
    }
@@ -21,13 +46,14 @@ class Header extends React.Component{
 	}
 }
 
-class Content extends React.Component {
+class TableRow extends React.Component{
 	render(){
-		return (
-			<div>
-				<h2>Content</h2>
-				<p>This is the content!</p>
-			</div>
+		return(
+			<tr>
+				<td>{this.props.data.id}</td>
+				<td>{this.props.data.name}</td>
+				<td>{this.props.data.age}</td>
+			</tr>
 		);
 	}
 }
