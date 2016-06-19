6032c9b988aff78c40ef2e0000976bbbf08f9a2e
Serge Bishyr <SeriyBg@gmail.com>
2016-06-19 14:36:19 +0300

    Add test dependencies + fake test

---
+++ test/test.js
@@ -0,0 +1,9 @@
+describe('Array', () => {
+  describe('#indexOf()', () => {
+    it('this test will pass', () => {
+    });
+	it('this test will fail', () => {
+		throw new Error('Fail!');
+	});
+  });
+});
