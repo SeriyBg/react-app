d010a20354d6a4ba7d31b0d22b8733d3332fb659
Serge Bishyr <SeriyBg@gmail.com>
2016-07-10 13:16:58 +0300

    Add display data to screen

--- app/components/ConfirmBattle.js
+++ app/components/ConfirmBattle.js
@@ -3,7 +3,7 @@ import React from 'react';
 function ConfirmBattle(props){
   return props.isLoading
       ? <p>LOADING !</p>
-      : <p>CONFIRM BATTLE</p>;
+      : <p>CONFIRM BATTLE: <pre>{JSON.stringify(props, null, ' ')}</pre></p>;
 }
 
 export default ConfirmBattle;
